f19fd9f78eaba93aaac266a773d417550768ab45
Alain Hélaïli <alain@carbonhub.io>
2020-04-03 18:53:08 +0200

    Checking the terminate and stop dates

--- ripper.js
+++ ripper.js
@@ -1,16 +1,23 @@
 const core = require('@actions/core')
-// const moment = require('moment')
+const moment = require('moment')
 
 function processInstance (instance) {
   core.debug(`Processing instance ${instance.InstanceId}`)
   const owner = getTag(instance, 'OWNER')
   const name = getTag(instance, 'NAME')
   const stop = getTag(instance, 'STOP')
-  const terminate = getTag(instance, 'TERMINATE')
-
+  const terminate = getTag(instance, 'TERMINATE') 
+  
   core.debug(`Owner: ${owner}, Name: ${name}, Stop: ${stop}, Terminate: ${terminate}`)
-}
 
+  if (!isDateSet(terminate)) {
+    core.debug(`Terminate date not set`)
+  } 
+  
+  if (!isDateSet(stop)) {
+    core.debug(`Stop date not set`)
+  }
+}
 
 function getTag (instance, tag) {
   const ownerTag = instance.Tags.find(instanceTag => instanceTag.Key.toUpperCase() === tag)
@@ -19,6 +26,10 @@ function getTag (instance, tag) {
   } else {
     return ownerTag.Value
   }
+}
+
+function isDateSet(date) {
+  return !(!terminate || terminate.toUpperCase() !== 'NEVER' || !moment(terminate).isValid()) 
 }
 
 module.exports = { processInstance }
