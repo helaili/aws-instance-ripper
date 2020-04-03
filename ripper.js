5373f68b6a84a60524a859ebe295d11b365976d0
Alain Hélaïli <alain@carbonhub.io>
2020-04-03 19:14:21 +0200

    Manage the dates

--- ripper.js
+++ ripper.js
@@ -1,21 +1,38 @@
 const core = require('@actions/core')
 const moment = require('moment')
 
-function processInstance (instance) {
+function processInstance (instance, ripperConfig) {
   core.debug(`Processing instance ${instance.InstanceId}`)
   const owner = getTag(instance, 'OWNER')
   const name = getTag(instance, 'NAME')
   const stop = getTag(instance, 'STOP')
   const terminate = getTag(instance, 'TERMINATE') 
-  
+  const tags = []
+
   core.debug(`Owner: ${owner}, Name: ${name}, Stop: ${stop}, Terminate: ${terminate}`)
 
   if (!isDateSet(terminate)) {
     core.debug(`Terminate date not set`)
+    if (ripperConfig.defaultTerminateDelay) {
+      const tag = {
+        Key: 'Terminate',
+        Value: moment().add(ripperConfig.defaultTerminateDelay, 'days').format('YYYY-MM-DD')
+      }
+      tags.push(tag)
+      core.debug(`Terminate now set to ${tag.Value}`)
+    }
   } 
   
   if (!isDateSet(stop)) {
     core.debug(`Stop date not set`)
+    if (ripperConfig.defaultStopDelay) {
+      const tag = {
+        Key: 'Stop',
+        Value: moment().add(ripperConfig.defaultStopDelay, 'days').format('YYYY-MM-DD')
+      }
+      tags.push(tag)
+      core.debug(`Stop now set to ${tag.Value}`)
+    }
   }
 }
 
