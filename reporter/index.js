af13dad83db485231f67c84ee6db59d31e720b37
Alain Hélaïli <alain@carbonhub.io>
2020-04-04 10:33:50 +0200

    Reorganize and implement a pinning workflow

--- reporter/index.js
+++ reporter/index.js
@@ -2,15 +2,31 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
+  const ghToken = core.getInput('token')
+  const octokit = new github.GitHub(ghToken)
+  const context = github.context
+
   const data = JSON.parse(core.getInput('data'))
   const regions = Object.getOwnPropertyNames(data)
   let report = ''
+  const labels = []
+  
+  if (core.getInput('label')) {
+    labels.push(core.getInput('label'))
+  }
 
   if (regions) {
-    regions.forEach(region => {
+    for (const region of regions) {
       core.debug(`Processing ${region}`)
       let regionTable = generateRegionTable(data[region])
       report = report.concat(`### [${region}](https://console.aws.amazon.com/ec2/v2/home?region=${region}#Instances:sort=tag:Name)\n${regionTable}\n`)
+    }
+
+    octokit.issues.create({
+      ...context.repo,
+      title: 'AWS Ripper report',
+      body: report,
+      labels: labels
     })
   } else {
     core.warning('Nothing to report')
@@ -27,11 +43,57 @@ function generateRegionTable(reportData) {
   core.debug(`Generating table for ${reportData}`)
 
   if (reportData.length !== 0) {
-    markdownTable = `Instance ID|Name|Owner|Stop|Terminate\n-|-|-|-|-\n`
+    markdownTable = `Name|Owner|Stop|Terminate|Instance ID\n-|-|-|-|-\n`
   }
   reportData.forEach(instance => {
-    markdownTable = markdownTable.concat(`${instance.instanceId}|${instance.name.value}|${instance.owner.value}|${instance.stop.value}|${instance.terminate.value}\n`)
+    markdownTable = markdownTable.concat(`${formatName(instance)}|${formatOwner(instance)}|${formatStop(instance)}|${formatTerminate(instance)}|${instance.instanceId}\n`)
   })
 
   return markdownTable
+}
+
+function formatName(instance) {
+  if (!instance.name.value || instance.name.value === 'null') {
+    return ':scream:'
+  } else {
+    return instance.name.value
+  }
+}
+
+function formatOwner(instance) {
+  if (!instance.owner.value || instance.owner.value === 'null') {
+    return ':ghost:'
+  } else {
+    return instance.owner.value
+  }
+}
+
+function formatStop(instance) {
+  if (!instance.stop.value || instance.stop.value === 'null') {
+    return ':fearful:'
+  } else {
+    let date = instance.stop.value
+    if (instance.stop.notSet) {
+      date = `:new: ${date}`
+    }
+    if (instance.stop.now) {
+      date = `:stop_sign: ${date}`
+    }
+    return date
+  }
+}
+
+function formatTerminate(instance) {
+  if (!instance.terminate.value || instance.terminate.value === 'null') {
+    return ':fearful:'
+  } else {
+    let date = instance.terminate.value
+    if (instance.terminate.notSet) {
+      date = `:new: ${date}`
+    }
+    if (instance.terminate.now) {
+      date = `:skull_and_crossbones: ${date}`
+    }
+    return date
+  }
 }
